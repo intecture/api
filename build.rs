f76ba7e2616b5fc18943b51d1b1b58032bb70c78
Pete <pete@hayes.id.au>
2015-10-15 12:26:50 +0100

    Copyright notice

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
+// Copyright 2015 Intecture Developers. See the COPYRIGHT file at the
+// top-level directory of this distribution and at
+// https://intecture.io/COPYRIGHT.
+//
+// Licensed under the Mozilla Public License 2.0 <LICENSE or
+// https://www.tldrlegal.com/l/mpl-2.0>. This file may not be copied,
+// modified, or distributed except according to those terms.
+
 fn main() {
     println!("cargo:rustc-link-search=native=/usr/local/lib");
 }
--- src/ffi/command.rs
+++ src/ffi/command.rs
@@ -1,3 +1,11 @@
+// Copyright 2015 Intecture Developers. See the COPYRIGHT file at the
+// top-level directory of this distribution and at
+// https://intecture.io/COPYRIGHT.
+//
+// Licensed under the Mozilla Public License 2.0 <LICENSE or
+// https://www.tldrlegal.com/l/mpl-2.0>. This file may not be copied,
+// modified, or distributed except according to those terms.
+
 use command::{Command, CommandResult};
 use libc::{c_char, c_void};
 use std::{convert, str};
