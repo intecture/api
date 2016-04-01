20d0412379a041ba98457100c04e776cc928545e
Pete Hayes <pete@hayes.id.au>
2016-04-01 16:18:55 +0100

    Add CZMQ lib to Travis build

--- .travis_install.sh
+++ .travis_install.sh
@@ -2,8 +2,17 @@
 
 git clone https://github.com/zeromq/zeromq4-1
 cd zeromq4-1
-git checkout "tags/v4.1.3"
+git checkout "tags/v4.1.4"
 ./autogen.sh
 ./configure --with-libsodium=no
 make -j 8
 sudo make install
+cd ..
+
+git clone https://github.com/zeromq/czmq
+cd czmq
+./autogen.sh
+./configure
+make
+sudo make install
+cd ..
