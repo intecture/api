2467a6bd02a15c135aa7e9a1d6c9f216865dc2fb
Pete Hayes <pete@hayes.id.au>
2016-12-08 11:19:41 +0000

    Copy lib[c]zmq.so symlinks as hardcoding the version number is not very portable

--- package.sh
+++ package.sh
@@ -125,12 +125,12 @@ main() {
     cp "$_cargodir/bindings/c/inapi.h" "$_pkgdir/inapi.h"
 
     # ZeroMQ assets
-    cp "$libdir/libzmq.so.5.1.0" "$_pkgdir/lib/"
+    cp "$libdir/libzmq.so" "$_pkgdir/lib/"
     cp "$libdir/pkgconfig/libzmq.pc" "$_pkgdir/lib/pkgconfig/"
     cp "$prefix/include/zmq.h" "$_pkgdir/include/"
 
     # CZMQ assets
-    cp "$libdir/libczmq.so.4.0.0" "$_pkgdir/lib/"
+    cp "$libdir/libczmq.so" "$_pkgdir/lib/"
     cp "$libdir/pkgconfig/libczmq.pc" "$_pkgdir/lib/pkgconfig/"
     cp "$prefix/include/czmq.h" "$_pkgdir/include/"
     cp "$prefix/include/czmq_library.h" "$_pkgdir/include/"
